15be380268df0223edd255e011e23612acf2748e
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2023-03-04 16:17:41 -0300

    feat: Added PEP8 line breaking examples

---
+++ pep8/line_breaking.py
@@ -0,0 +1,93 @@
+""" This module shows examples of how to break likes when our code passes the
+    79 character limit.
+"""
+
+
+def first_function_with_very_long_name(
+        first_argument: str,
+        second_argument: int) -> str:
+    """ This function would pass the 79 character limit.
+    """
+    return f"{first_argument} {second_argument}"
+
+
+def second_function_with_very_long_name(
+        first_argument: str,
+        second_argument: int
+        ) -> str:
+    """ This function would pass the 79 character limit.
+    """
+    return f"{first_argument} {second_argument}"
+
+
+def third_function_with_very_long_name(
+        first_argument: str, second_argument: int) -> str:
+    """ This function would pass the 79 character limit.
+    """
+    return f"{first_argument} {second_argument}"
+
+
+def fourth_function_with_very_long_name(
+                                        first_argument: str,
+                                        second_argument: int) -> str:
+    """ This function would pass the 79 character limit.
+    """
+    return f"{first_argument} {second_argument}"
+
+
+def fifth_function_with_very_long_name(first_argument: str,
+                                       second_argument: int) -> str:
+    """ This function would pass the 79 character limit.
+    """
+    return f"{first_argument} {second_argument}"
+
+
+def function_with_math_operators(
+        first_long_argument, second_long_argument,
+        third_long_argument, fourth_long_argument):
+    """ This function content would pass the 79 character limit.
+    """
+    # It is recommended that we break we place the math operators at the
+    # beginning of the line, not at the end of the line.
+    return (first_long_argument
+            + second_long_argument
+            - third_long_argument
+            * fourth_long_argument)
+
+
+def function_example_with_backslash(number_to_check):
+    """ This function content would pass the 79 character limit.
+    """
+    return number_to_check < 10 or 21 <= number_to_check < 20 or \
+        number_to_check > 100
+
+
+def function_with_if_examples(number_to_check):
+    """ This function content would pass the 79 character limit.
+    """
+    # Notice that the conditions aren't at the same level of parenthesis
+    if (
+            number_to_check < 10 or
+            21 <= number_to_check < 20 or
+            number_to_check > 100):
+        return True
+    # Notice that the closing parenthesis is at the same level of the oppening
+    # parenthesis
+    if (
+            number_to_check < 10 or
+            21 <= number_to_check < 20 or
+            number_to_check > 100
+       ):
+        return True
+    # Notice that the closing parenthesis is at the same level of the if clause
+    if (
+            number_to_check < 10 or
+            21 <= number_to_check < 20 or
+            number_to_check > 100
+    ):
+        return True
+    # This is my least preferred one
+    if (number_to_check < 10 or
+            21 <= number_to_check < 20 or
+            number_to_check > 100):
+        return True
