11b3d38cc1de6306ebaccdd8eb45eb19a737a2ee
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2023-03-17 10:18:18 -0300

    feat: Added Open/Close Principle example where classes are tightly coupled

---
+++ solid/open_closed_principle_ocp/classes_tightly_coupled.py
@@ -0,0 +1,40 @@
+""" This module has an example of the Open/Close Principle in Python where
+    classes are tightly coupled.
+"""
+
+
+class UserBadExample():
+    """ This is an example of a class in Python that is NOT following the
+        Open/Closed Principle
+    """
+    def __init__(self, username, name):
+        self.username = username
+        self.name = name
+
+
+# This function is tightly coupled to the UserBadExample class as we would need
+# to change the get_user_bad_example function if we need to change the
+# UserBadExample class. That also violates the Open/Close Principle.
+def get_user_bad_example(user_bad_example):
+    """ This is an example of a function in Python that is NOT following the
+        Open/Closed Principle
+    """
+    return {
+                "username": user_bad_example.username,
+                "name": user_bad_example.name
+           }
+
+
+# Transforming the function into a method of the class solves the previous
+# problem and follows the Open/Close Principle.
+class User():
+    """ This is an example of a class in Python that follows the
+        Open/Closed Principle
+    """
+    def __init__(self, username, name):
+        self.username = username
+        self.name = name
+
+    def get(self):
+        """ Get user data """
+        return {"username": self.username, "name": self.name}
