2653f645f22b5aeca6f406e41a99236a0e8d7047
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2023-03-19 08:16:18 -0300

    feat: Added examples of the Interface Segregation Principle (ISP)

---
+++ solid/interface_segregation_principle_isp.py
@@ -0,0 +1,98 @@
+""" This module has examples of the Interface Segregation Principle (ISP)
+    in Python.
+"""
+
+
+from abc import ABC, abstractmethod
+
+
+class EmployeeBadExample(ABC):
+    """ An abstract class example that doesn't follow the Interface Segregation
+        Principle as it has methods that aren't used by its child classes
+    """
+
+    @abstractmethod
+    def write_code(self):
+        """ Write code method
+        """
+
+    @abstractmethod
+    def cook(self):
+        """ Cook method
+        """
+
+    @abstractmethod
+    def lead(self):
+        """ Lead method
+        """
+
+
+class ChefBadExample(EmployeeBadExample):
+    """ Chef Class
+    """
+    def write_code(self):
+        raise NotImplementedError("Chefs don't write code.")
+
+    def cook(self):
+        print("I am cooking")
+
+    def lead(self):
+        print("I am leading")
+
+
+# Those classes from now on follow the Interface Segregation Principle
+# Instead of having one abstract class for all employees, we divided them by
+# roles and the child class uses multiple inheritance.
+class Cook(ABC):
+    """ An abstract class representing a cook (person who cooks)
+    """
+    @abstractmethod
+    def cook(self):
+        """ Cook method
+
+        """
+
+
+class Leader(ABC):
+    """ An abstract class representing a leader
+    """
+
+    @abstractmethod
+    def lead(self):
+        """ Lead method
+        """
+
+
+class Chef(Cook, Leader):
+    """ Chef Class
+    """
+    def lead(self):
+        print("I am leading")
+
+    def cook(self):
+        print("I am cooking")
+
+
+# Alternatively, the method could be implemented directly on the child class
+# without relating it to the parent class. The fly method of the Bird class
+# below is an example of this approach.
+class Animal(ABC):
+    """ An abstract class representing a leader
+    """
+
+    @abstractmethod
+    def eat(self):
+        """ Eat method
+        """
+
+
+class Bird(Animal):
+    """ Bird Class
+    """
+    def eat(self):
+        print("I am eating")
+
+    def fly(self):
+        """ Fly method
+        """
+        print("I am flying")
