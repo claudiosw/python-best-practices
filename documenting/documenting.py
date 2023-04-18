1d4207e97534343ebd1b6b59d0bda18cc9fcf3f4
Claudio Shigueo Watanabe <claudiosw@gmail.com>
2023-04-18 10:49:29 -0300

    improve: Minor grammar improvements

--- documenting/documenting.py
+++ documenting/documenting.py
@@ -1,8 +1,9 @@
 """ This class is an example on how to document Python code
 """
 
-def transform_int_to_str (number: int) -> str:
-    """ Transform a int to a string
+
+def transform_int_to_str(number: int) -> str:
+    """ Transform a int into a string
 
     :param number: number to transform
     :returns: A string with 'The input was ' concatenated with the parameter
@@ -14,18 +15,18 @@ class Transformer():
     """
     Small description of the class
 
-    Larger description of the class. This class is just an example to show how
-    to document Python code.
+    Larger description of the class. This class is just an example of how to
+    document Python code.
     """
-    def transform_float_less_than_100_to_int (self, number: float) -> int:
-        """ Transform a number to string
+    def transform_float_less_than_100_to_int(self, number: float) -> int:
+        """ Transform a number into a string
 
         :param number: number to transform
-        :raises ValueError: if the parameter is greater or equal to 100
+        :raises ValueError: if the parameter is greater than or equal to 100
         :returns: A string with 'The input was ' concatenated with the
         parameter
         """
         if number < 100:
             return int(number)
         else:
-            raise ValueError(f"{number} is greater or equal to 100")
+            raise ValueError(f"{number} is greater than or equal to 100")
